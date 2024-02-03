f57877a5b8d34a7c31396d429315780f30ee1f2b
pksui <289184538@qq.com>
2024-02-03 23:42:14 +0800

    2024-02-03

---
+++ 3232.py
@@ -0,0 +1,23 @@
+a = 1.7
+b = a
+
+print(int(b))
+
+
+c = 3
+
+c *= -3
+
+print(c)
+
+from settings import Settings
+
+a = Settings()
+
+print(a.bullet_speed)
+
+
+print(round(5,-1))
+
+for i in range(3):
+    print(i)
